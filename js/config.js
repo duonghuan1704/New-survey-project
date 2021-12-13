9b9afcf0a93b8639c1f55a243a90cb4d529fcdd1
= <=>
2021-12-13 18:12:03 +0700

    first commit

---
+++ js/config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    titleWeb: ":))))",
+    introTitle: ' Hiii Em ',
+    introDesc: `Nhờ em giúp anh trả lời câu hỏi này nhé `,
+    btnIntro: 'Yes :">>>',
+    title: 'Em có muốn làm người yêu anh không!!! :)))',
+    desc: 'Không chọn câu trả lời hoặc thoát ra tức là đồng ý làm người yêu anh:)) ',
+    btnYes: 'Có anh',
+    btnNo: 'Không. Anh mơ à :3',
+    question: 'Ỏ em đúng là một cô gái đáng yêu đó. Ghi lý do đồng ý đi :>',
+    btnReply: 'Gửi đi',
+    reply: 'Chim hỏi lũ sâu xanh-Lá có gì mà thích-Lũ sâu cười khúc khích-Thích đâu cần lý do <3',
+    mess: 'Hihi. Một ngày vui vẻ. Cho anh làm quen em nhé :))',
+
+    messLink: 'https://www.messenger.com/t/100005400932856' 
+}
